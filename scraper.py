36fadd3049520b52165ea59e8ba9037ef4bc3139
Raiden-Official <raidzenash@gmail.com>
2019-08-03 13:59:53 +0530

    Fixed movie_summary , JSON file initialization with []

--- scraper.py
+++ scraper.py
@@ -7,11 +7,18 @@ import sys
 
 # The Godfather
 # url = 'https://www.imdb.com/title/tt0068646/'
+
+# Avengers : Endgame
+# url = 'https://www.imdb.com/title/tt4154796/'
+
+# Comment the below line if you want to use the above urls to fetch data.
 url = sys.argv[1]
+
+# Get content on the wesite by posting request.
 response = requests.get(url, timeout = 5)
 content = BeautifulSoup(response.content, 'html.parser')
 
-movie_summary_text = content.find_all('div', class_='summary_text')[0].string
+movie_summary_text = content.find('div', class_='summary_text').text.strip()
 movie_credit_details = content.find_all('div', class_='credit_summary_item')
 
 movie_director_name = movie_credit_details[0].find_all('a')[0].string
@@ -39,7 +46,7 @@ movie_genres = []
 movie_genre_and_release_date = movie_details.find_all('span', class_='ghost')[1].find_next_siblings('a')
 for tags in movie_genre_and_release_date:
     if re.match('(^[0-9])', tags.string):
-        movie_release_date = tags.string
+        movie_release_date = tags.string.strip()
     else:
         movie_genres.append(tags.string)
 
@@ -47,7 +54,8 @@ movie_poster_url = content.find('div', class_='poster').contents[1].contents[1][
 movie_label = content.find('h1', class_='').contents
 movie_name = movie_label[0].strip()
 movie_year = movie_label[1].find('a').string
-# Print the movie name in cmd:
+
+# Print the movie name in cmd and create an object from the scraped data:
 print("Getting data for movie: "+movie_name)
 movieJson = {}
 movieJson['name'] = movie_name
@@ -62,9 +70,13 @@ movieJson['director'] = movie_director_name
 movieJson['writers'] = movie_writer_names
 movieJson['stars'] = movie_star_names
 
-with open('movies.json', 'r') as f:
-    movies = json.load(f)
-    
+# Note: r+ mode appends data in front of the old data 
+with open('movies.json', 'r+') as f:
+    try:
+        movies = json.load(f)
+    except:
+        json.dump([], f)
+        movies = []
 with open('movies.json', 'w') as f:
     # check if data doesen't exist already and then update
     exists =  False
